727ddd8457221127759959ff72d53c848f8ea79b
TrHien <ttthuhien0104@gmail.com>
2022-01-25 21:31:53 +0200

    make it better

--- server.js
+++ server.js
@@ -1,33 +1,30 @@
 const express = require('express')
-const request = require('request')
-
 const app = express()
+const cors = require('cors')
 
-const path = require('path')
+const PORT = process.env.PORT || 3000
 
-app.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', '*')
-   next()
-})
-
-app.get('/rps/history', (req, res) => {
-   request({ url: 'https://bad-api-assignment.reaktor.com/rps/history' }, (error, response, body) => {
-      if (error || response.statusCode !== 200) {
-         return res.status(500).json({ type: 'error', message: error.message })
-      }
-
-      res.json(JSON.parse(body))
-   })
-})
-
-if (process.env.NODE_ENV === 'production') {
-   app.use(express.static())
-   app.get('*', (req, res) => {
-      req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-   })
+if (process.env.NODE_ENV !== 'production') {
+   require('dotenv').config()
 }
 
-const PORT = process.env.PORT || 3003
+const domainsFromEnv = process.env.CORS_DOMAINS || ''
+const whitelist = domainsFromEnv.split(',').map((item) => item.trim())
+
+const corsOptions = {
+   origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+         callback(null, true)
+      } else {
+         callback(new Error('Not allowed by CORS'))
+      }
+   },
+
+   credentials: true,
+}
+
+app.use(cors(corsOptions))
+
 app.listen(PORT, (err) => {
    if (err) return console.log(err)
    console.log(`listening on ${PORT}`)
